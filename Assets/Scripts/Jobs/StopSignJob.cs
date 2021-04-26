0cf1025ac946f15ac8223b16ff513dbc957ffda0
MichaelHaughian <32159629+MichaelHaughian@users.noreply.github.com>
2021-04-26 23:43:40 +0100

    stopped stopjob dwarf occupying spaces they were no longer in

--- Assets/Scripts/Jobs/StopSignJob.cs
+++ Assets/Scripts/Jobs/StopSignJob.cs
@@ -6,6 +6,7 @@ public class StopSignJob : DwarfJob
 {
     private BaseDwarf dwarf;
     private JobType type = JobType.STOP;
+    private Vector3Int cellPlantedFeet;
 
     public override float SobrietyScale { get { return 0.6f; } }
 
@@ -13,8 +14,18 @@ public class StopSignJob : DwarfJob
     {
         dwarf.SnapToCurrentCell();
 
-        if (!GameController.TilemapController.IsCellOccupiedWithDwarf(dwarf.CurrentCell)) {
+        Debug.Log("CURRENT CELL:" + dwarf.CurrentCell);
+        Debug.Log("CELL PLANTED FEET:" + cellPlantedFeet);
+        if (dwarf.CurrentCell != cellPlantedFeet) {
+            GameController.TilemapController.UnoccupyCellWithDwarf(cellPlantedFeet);
+            cellPlantedFeet = dwarf.CurrentCell;
             GameController.TilemapController.OccupyCellWithDwarf(dwarf.CurrentCell);
+        }
+        else {
+            if (!GameController.TilemapController.IsCellOccupiedWithDwarf(dwarf.CurrentCell)) {
+                cellPlantedFeet = dwarf.CurrentCell;
+                GameController.TilemapController.OccupyCellWithDwarf(dwarf.CurrentCell);
+            }
         }
         
         //add animation for dwarf
@@ -26,6 +37,7 @@ public class StopSignJob : DwarfJob
     public override void InitializeJobAction(BaseDwarf incDwarf, Vector3Int currentCell)
     {
         dwarf = incDwarf;
+        cellPlantedFeet = dwarf.CurrentCell;
         dwarf.JobIcon.SetStopSignIcon();
         if(GameController.Score >= Constants.stopSignCost) {
             GameController.AddToScore(-Constants.stopSignCost);
