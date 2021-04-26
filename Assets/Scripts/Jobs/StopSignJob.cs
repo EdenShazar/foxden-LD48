9bb7643fd49cf2ec6c032a707b1ddef7b682a9e9
MichaelHaughian <32159629+MichaelHaughian@users.noreply.github.com>
2021-04-27 00:15:37 +0100

    fixed stopjob being purchasable with no money

--- Assets/Scripts/Jobs/StopSignJob.cs
+++ Assets/Scripts/Jobs/StopSignJob.cs
@@ -12,32 +12,36 @@ public class StopSignJob : DwarfJob
 
     public override bool JobAction(DwarfSurroundings surroundings)
     {
-        dwarf.SnapToCurrentCell();
+        if (GameController.Score >= Constants.stopSignCost) {
+            dwarf.SnapToCurrentCell();
 
-        if (dwarf.CurrentCell != cellPlantedFeet) {
-            GameController.TilemapController.UnoccupyCellWithDwarf(cellPlantedFeet);
-            cellPlantedFeet = dwarf.CurrentCell;
-            GameController.TilemapController.OccupyCellWithDwarf(dwarf.CurrentCell);
-        }
-        else {
-            if (!GameController.TilemapController.IsCellOccupiedWithDwarf(dwarf.CurrentCell)) {
+            if (dwarf.CurrentCell != cellPlantedFeet) {
+                GameController.TilemapController.UnoccupyCellWithDwarf(cellPlantedFeet);
                 cellPlantedFeet = dwarf.CurrentCell;
                 GameController.TilemapController.OccupyCellWithDwarf(dwarf.CurrentCell);
             }
+            else {
+                if (!GameController.TilemapController.IsCellOccupiedWithDwarf(dwarf.CurrentCell)) {
+                    cellPlantedFeet = dwarf.CurrentCell;
+                    GameController.TilemapController.OccupyCellWithDwarf(dwarf.CurrentCell);
+                }
+            }
+
+            //add animation for dwarf
+            dwarf.animator.StopSign();
         }
-        
-        //add animation for dwarf
-        dwarf.animator.StopSign();
 
         return true;
+        
     }
 
     public override void InitializeJobAction(BaseDwarf incDwarf, Vector3Int currentCell)
     {
         dwarf = incDwarf;
-        cellPlantedFeet = dwarf.CurrentCell;
-        dwarf.JobIcon.SetStopSignIcon();
+        
         if(GameController.Score >= Constants.stopSignCost) {
+            cellPlantedFeet = dwarf.CurrentCell;
+            dwarf.JobIcon.SetStopSignIcon();
             GameController.AddToScore(-Constants.stopSignCost);
         }
         
